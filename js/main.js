0cfb273a29ca564351e3e62ea34ca82e8ac2a3a3
Anadbh <anafruiz20@gmail.com>
2021-02-04 16:15:57 +0100

    Add new functions and attribute html

--- js/main.js
+++ js/main.js
@@ -1,45 +1,38 @@
-`use stric`
+`use stric`;
 
-
-const buttonElement= document.querySelector(".js-button");
-const inputElement=document.querySelector(".js-input");
-const inputClue=document.querySelector(".js-clue");
-const tryElement=document.querySelector(".js-try");
-const numberRandom=getRandomNumber(100);
-let intentos=0;
-
-
+const buttonElement = document.querySelector(".js-button");
+const inputElement = document.querySelector(".js-input");
+const inputTips = document.querySelector(".js-clue");
+const tryElement = document.querySelector(".js-try");
+const numberRandom = getRandomNumber(100);
+let tries = 0;
+function getRandomNumber(max) {
+  const RandomNumber = Math.ceil(Math.random() * max);
+  return RandomNumber;
+}
+function writteTips(message) {
+  inputTips.innerHTML = "Pista:" + message;
+}
 console.log(numberRandom);
 
-function handleButton (){
-const selectValue= parseInt(inputElement.value);
+function handleButton() {
+  const selectValue = parseInt(inputElement.value);
 
-if (selectValue===numberRandom){
-    inputClue.innerHTML='Has ganado campeona'
-}
-else if(selectValue<1 ||selectValue>100){
-    inputClue.innerHTML='El número debe estar entre 1 y 100'
-}
-else if(selectValue<numberRandom){
-    inputClue.innerHTML='Demasiado bajo'
-
-}
-else if(selectValue>numberRandom){
-    inputClue.innerHTML='Demasiado alto'
-
+  if (selectValue === numberRandom) {
+    writteTips("Has ganado campeona!!!");
+  } else if (selectValue < 1 || selectValue > 100) {
+    writteTips("El número debe estar entre 1 y 100");
+  } else if (selectValue < numberRandom) {
+    writteTips("El número es demasiado bajo");
+  } else if (selectValue > numberRandom) {
+    writteTips("El número es demasiado alto");
+  }
+  triesNumber();
 }
 
-intentos ++;
-tryElement.innerHTML = 'Número de intentos :'+ intentos;
-
+function triesNumber() {
+  tries++;
+  tryElement.innerHTML = "Número de intentos :" + tries;
 }
 
-function getRandomNumber (max) {
-const RandomNumber=Math.ceil(Math.random()*max);
-return RandomNumber;
-
-}
-
-
-
-buttonElement.addEventListener('click',handleButton)
+buttonElement.addEventListener("click", handleButton);
